f8cd9651cd9281c530d754fb00a0db7e70a6ae7a
kojiman-git <kojitatu3@gmail.com>
2025-01-10 00:48:33 +0900

    不要なファイルを削除

--- src/app/Home.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'; // これを追加
-import { render, screen } from '@testing-library/react';
-import Home from './page';
-
-test('renders welcome message', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/ようこそ、Next.jsの世界へ！/i);
-  expect(linkElement).toBeInTheDocument();
-});
