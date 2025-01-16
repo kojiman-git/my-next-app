075d91d6bca1acde5445e503ff1cce561f9ead53
kojiman-git <kojitatu3@gmail.com>
2025-01-16 22:37:02 +0900

    メモアプリにMaterial UIを導入したバージョンを実装

--- src/data/nav.js
+++ src/data/nav.js
@@ -14,4 +14,5 @@ export const navList = [
   "080_my-task-app_DisplayConditions",
   "080_my-task-app_DisplayConditions_Material_UI",
   "090_my-memo-app_ComponentSegmentation",
+  "090_my-memo-app_ComponentSegmentation_Material_UI",
 ];
